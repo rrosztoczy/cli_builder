23820176fbff48dc4d2305f4249a027b36ce6dd0
rrosztoczy <rrosztoczy@gmail.com>
2019-04-18 09:03:27 -0400

    done

--- lib/cli_builder/menu.rb
+++ lib/cli_builder/menu.rb
@@ -4,13 +4,14 @@ module CliBuilder
     #TODO: Fix tests and add more tests (4 hours)
     #TODO: Refactor main menu area (2 hours), think about private etc
     #TODO: Build Crud section (8 hours)
+    #TODO: Add formatting options(title, layouts?), default formatting clears screen (2 hours)
     #TODO: Add readme (2 hours)
     #TODO: Add readme (2 hours)
     #TODO: Fix project organization
  
-
+    
     class Menu
-        attr_accessor :title, :menu_options, :parent
+        attr_accessor :title, :menu_options, :parent, :previous_menu_option, :main_menu_option
         @@all = [];
         @@main_menu = {};
 #******************************************Initialize and set key variables*************************************
@@ -27,7 +28,7 @@ module CliBuilder
             @@main_menu = menu
         end
 
-        def assignParents(menu_options)
+        def assign_parents(menu_options)
             menu_options.each do | menu_option | 
                 if menu_option.class == CliBuilder::Menu
                     menu_option.parent = self
@@ -39,20 +40,27 @@ module CliBuilder
             #TODO: Review on the usecase... but this may not be necessary
             self.title = title.downcase.gsub(/\s+/,"_").downcase.to_sym
             self.menu_options = menu_options
-            assignParents(menu_options)
+            self.assign_parents(self.menu_options)
+            self.previous_menu_option = menu_options.length + 1
+            self.main_menu_option = menu_options.length + 2
             Menu.all << self
             Menu.main_menu = self
         end
 
 # **************************************Menu Print and Build Methods********************************************************************
+
+        def titlecase(string)
+            string.split("_").each {|word| word.capitalize!}.join(" ")
+        end
+
+        def menu_option_number(index)
+            index + 1
+        end
+
         def build_menu
             self.build_menu_title
             self.build_menu_body
             self.build_application
-        end
-
-        def titlecase(string)
-            string.split("_").each {|word| word.capitalize!}.join(" ")
         end
 
         def build_menu_title
@@ -63,15 +71,15 @@ module CliBuilder
         def build_menu_body
             menu_options.each_with_index do |menu_option, index| 
                 if menu_option.class == CliBuilder::Menu
-                    puts "#{index + 1}. #{titlecase(menu_option.title.to_s)}"
+                    puts "#{menu_option_number(index)}. #{titlecase(menu_option.title.to_s)}"
                 else
-                    puts "#{index + 1}. #{titlecase(menu_option.to_s)}"
+                    puts "#{menu_option_number(index)}. #{titlecase(menu_option.to_s)}"
                 end
             end
             #Do not add Back options to main menu (last menu created)
             if self != Menu.main_menu
-                puts "#{menu_options.length + 1}. Back to Previous Menu"
-                puts "#{menu_options.length + 2}. Back to Main Menu"
+                puts "#{previous_menu_option}. Back to Previous Menu"
+                puts "#{main_menu_option}. Back to Main Menu"
             end
                 puts ""
                 puts "Please enter your selection:"
@@ -85,29 +93,28 @@ module CliBuilder
 
         def build_application
             user_input = get_user_input.to_i
-            if !user_input.between?(1, menu_options.length+2)
+            if !user_input.between?(1, main_menu_option)
                 user_input_validation
             else
-                @@current_menu = self.menu_options[user_input-1]
                 execute_application_logic(user_input)
             end
         end
 
         def execute_application_logic(user_input)
             self.menu_options.each_with_index do |menu_option, index|
-                executable_menu_option = index + 1
-                previous_menu_option = menu_options.length + 1
-                main_menu_option = menu_options.length + 2
-
                 case user_input
-                when previous_menu_option
-                self.parent.build_menu
-                when main_menu_option
-                Menu.main_menu.build_menu
-                when executable_menu_option
-                if menu_option.class == CliBuilder::Menu
-                    menu_option.build_menu
+                when self.previous_menu_option
+                    system "clear" or system "cls"
+                    self.parent.build_menu
+                when self.main_menu_option
+                    system "clear" or system "cls"
+                    Menu.main_menu.build_menu
+                when self.menu_option_number(index)
+                    if menu_option.class == CliBuilder::Menu
+                        system "clear" or system "cls"
+                        menu_option.build_menu
                 else
+                    system "clear" or system "cls"
                     send(menu_option)
                     self.build_menu
                 end
